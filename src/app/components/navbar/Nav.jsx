8f56bd4583f5a9c37e4c5d754fc2adfbb90d89f1
Manuel Daviid Gomez Sanchez <manueldavidgomez2003@gmail.com>
2025-07-31 13:27:21 -0500

    Agrege una imagen para mejor visual y arregle bugs visuales

--- src/app/components/navbar/Nav.jsx
+++ src/app/components/navbar/Nav.jsx
@@ -3,32 +3,29 @@ import "./Nav.css";
 
 const Nav = () => {
   return (
-
-      <nav>
-        <input type="checkbox" id="active" />
-        <label for="active" class="menu-btn">
-          <span></span>
-        </label>
-        <label for="active" class="close"></label>
-        <div class="wrapper">
-          <ul>
-            <li>
-              <a href="/">INICIO</a>
-            </li>
-            <li>
-              <a href="/pages/builder">CONSTRUCTOR</a>
-            </li>
-            <li>
-              <a href="/pages/library">BIBLIOTECA</a>
-            </li>
-            <li>
-              <a href="/pages/acercade">ACERCA</a>
-            </li>
-          </ul>
-        </div>
-       
-      </nav>
-
+    <nav>
+      <input type="checkbox" id="active" />
+      <label for="active" class="menu-btn">
+        <span></span>
+      </label>
+      <label for="active" class="close"></label>
+      <div class="wrapper">
+        <ul>
+          <li>
+            <a href="/">INICIO</a>
+          </li>
+          <li>
+            <a href="/pages/builder">CONSTRUCTOR</a>
+          </li>
+          <li>
+            <a href="/pages/library">BIBLIOTECA</a>
+          </li>
+          <li>
+            <a href="/pages/acercade">ACERCA</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
   );
 };
 
--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,6 @@
+import Image from "next/image";
 import PagesLayout from "./pagesLayout";
+import heroImg from "../app/assets/hero.png";
 
 export default function Home() {
   return (
@@ -7,28 +9,30 @@ export default function Home() {
         <h1>
           CONSTRUYE TU STACK TECNOLÓGICO EN <strong>MINUTOS</strong>
         </h1>
-        <p className="text-center my-5"> lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
-            eiusmod tempor incididunt ut labore et <br/> dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea</p>
-        <input
-          type="text"
-          placeholder="Ingresa el nombre de tu proyecto"
-          className="border bg-white border-gray-300 rounded-2xl p-2 w-100 mt-6 text-gray-700 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
-        />
+        <p className="text-center my-5">
+          lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+          eiusmod tempor incididunt ut labore et <br /> dolore magna aliqua. Ut
+          enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
+          ut aliquip ex ea
+        </p>
+        <button className=" mx-auto block cursor-pointer px-6 py-2 bg-[#08080c73] border border-[#202944] text-white rounded-lg hover:bg-[#1b31573d] transition-colors duration-300">
+          Ver Ahora
+        </button>
       </section>
       <PagesLayout>
         <section className="flex flex-row flex-center items-center gap-10 my-30 mysection">
           <section>
-            lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea
+            <Image
+              src={heroImg}
+              alt="Hero Image"
+              title="Image by pngtree.com"
+              className="mask-b-from-20% mask-b-to-100%"
+            />
           </section>
 
           <section>
             <h2 className="font-bold">¿Porque usar Stacking?</h2>
-            <p className="text-gray-700 mb-4">
+            <p className="text-gray-700 mt-4">
               lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
               eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
               enim ad minim veniam, quis nostrud exercitation ullamco laboris
